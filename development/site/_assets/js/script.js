7cf44bafc26b77f840a4ad9ca7134de36b1842da
= <=>
2012-02-20 18:57:34 -0600

    finished header,footer,partial home

--- development/site/_assets/js/script.js
+++ development/site/_assets/js/script.js
@@ -4,7 +4,34 @@ http://compressorbot.com
 Copyright CompressorBot 2012 and beyond
 */
 $(function(){
+	var modalLink = $('.modal_link'),
+		modalClose = $('.modal_close'),
+		modalWindow = $('.modal_wrapper section'),
+		winWidth = $(window).width()
+	;
 
+	modalLink.on('click',function(e){
+		var that = $(this),
+			modalWin = $(that.attr('href')),
+			modalWrap = modalWin.parent()
+		;
+		// Set modal windows top/left location
+		modalWindow.css({
+			'top': (modalWrap.outerHeight()/2) - (modalWin.outerHeight()/2),
+			'left': (modalWrap.outerWidth()/2) - (modalWin.outerWidth()/2)
+		});
+		// Toggle Modal window on/off
+		modalWrap.toggle();
+		e.preventDefault();
+		return false;
+	});
+
+	modalClose.on('click',function(e){
+		var that = $(this);
+			that.parent().parent().toggle();
+		e.preventDefault();
+		return false;
+	});
 });
 
 
---
+++ development/site/_assets/templates/footer.inc.php
@@ -0,0 +1,29 @@
+<!-- FOOTER Section -->
+    <footer>
+      <div class="wrapper">
+        <ul>
+          <li>Copyright CompressorBot 2012</li>
+          <li><a href="#contact_modal">Contact Us</a></li>
+        </ul>
+    </div><!-- END .wrapper -->
+    </footer>
+</div> <!-- END #container DIV -->
+  <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="_assets/js/libs/jquery-1.7.1.min.js"><\/script>')</script>
+
+  <!-- SCRIPTS -->
+  <script defer src="_assets/js/plugins.js"></script>
+  <script defer src="_assets/js/min/script.min.js?v=1"></script>
+  <!-- END SCRIPTS -->
+
+  <!-- Asynchronous Google Analytics snippet. Change UA-XXXXX-X to be your site's ID.
+       mathiasbynens.be/notes/async-analytics-snippet -->
+  <script>
+    var _gaq=[['_setAccount','UA-29294898-1'],['_trackPageview']];
+    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+    s.parentNode.insertBefore(g,s)}(document,'script'));
+  </script>
+</body>
+</html>
