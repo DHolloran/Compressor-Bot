cb69fa9bcb92adbc56ee50e21375d9253101a73e
= <=>
2012-02-21 22:20:44 -0600

    decompress styled starting scripting

--- development/site/_assets/js/script.js
+++ development/site/_assets/js/script.js
@@ -7,9 +7,11 @@ $(function(){
 	var modalLink = $('.modal_link'),
 		modalClose = $('.modal_close'),
 		modalWindow = $('.modal_wrapper section'),
-		winWidth = $(window).width()
+		winWidth = $(window).width(),
+		toolSwitch = $('.tool_switch')
 	;
 
+/* ==== Modal Box Control (turn on modal) ==== */
 	modalLink.on('click',function(e){
 		var that = $(this),
 			modalWin = $(that.attr('href')),
@@ -20,18 +22,31 @@ $(function(){
 			'top': (modalWrap.outerHeight()/2) - (modalWin.outerHeight()/2),
 			'left': (modalWrap.outerWidth()/2) - (modalWin.outerWidth()/2)
 		});
-		// Toggle Modal window on/off
+		// Toggle Modal window on if it is off
 		modalWrap.toggle();
 		e.preventDefault();
 		return false;
 	});
-
+/* ==== Modal Box Control (turn off modal) ==== */
 	modalClose.on('click',function(e){
 		var that = $(this);
+			// Toggle modal window off if it is on
 			that.parent().parent().toggle();
 		e.preventDefault();
 		return false;
 	});
+/* ==== Tool Control (switch to upload)==== */
+toolSwitch.on('click', function(e){
+	var that = $(this);
+	$(that.attr('href')).show();
+	console.log($('#'+that.data('tool')).hide());
+
+
+	e.preventDefault();
+	return false;
+});
+/* ==== Tool Control (switch to insert)==== */
+/* ==== Language/Options Control (change options for each language) ==== */
 });
 
 
