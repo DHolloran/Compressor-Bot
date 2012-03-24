97d39b2c91e782a19a3f3676d5ffa370f4ab86d0
Dan Holloran <2011-10-26holloran@gmail.com>
2012-03-24 11:10:07 -0500

    add flow analytics

--- _assets/includes/controller/views/modals/register_modal.inc.php
+++ _assets/includes/controller/views/modals/register_modal.inc.php
@@ -13,17 +13,6 @@
 				<input type="password" name="register_password1" placeholder="password" class="pass1"><br>
 				<input type="password" name="register_password2" placeholder="re-enter password" class="pass2"><br>
 				<span class="error_field"></span>
-				<input type="hidden" name="cmd" class="paypal1" value="_s-xclick">
-				<input type="hidden" name="hosted_button_id" class="paypal2" value="V9GVS7ACWNXN2">
-				<table>
-				<tr><td><input type="hidden" name="on0" value=""></td></tr>
-				<tr><td><select name="os0" class="paypal3">
-					<option value="Monthly">Monthly : $1.99USD - monthly</option>
-					<option value="Yearly">Yearly : $19.99USD - yearly</option>
-					<option value="Basic">Basic : $0.00USD - yearly</option>
-				</select> </td></tr>
-				</table>
-				<input type="hidden" name="currency_code" class="paypal4" value="USD">
 				<input type="image" src="<?php echo "{$assets}/img/buttons/register_btn.png"; ?>" border="0" name="submit" alt="REGISTER">
 				<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 				</form>
