e192aa59daf864369958ca98770e07b779f8bb53
Dan Holloran <dtholloran@gmail.com>
2013-03-30 07:43:34 -0500

    Added html beautifier to language options toggle

--- compressorbot/assets/js/beautifier.js
+++ compressorbot/assets/js/beautifier.js
@@ -4,6 +4,6 @@
 var lbOptionsRadio = $('#lb_options input[type="radio"]');
 lbOptionsRadio.on('change', function(){
 	var showId = $(this).val();
-	$('.cb-options-section, .jb-options-section').addClass('hide');
+	$('.cb-options-section, .jb-options-section, .hb-options-section').addClass('hide');
 	$('.'+showId).removeClass('hide');
 });
