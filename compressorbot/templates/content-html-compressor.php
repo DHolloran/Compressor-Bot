4c35d6c40d6fd8a8e54edf12718a3123fa0e15e0
Dan Holloran <dtholloran@gmail.com>
2013-03-30 09:59:16 -0500

    Added a little testing code to the text areas

--- compressorbot/templates/content-html-compressor.php
+++ compressorbot/templates/content-html-compressor.php
@@ -2,7 +2,15 @@
 	<h2>Compress Your HTML <span class="muted">powered by <a href="" target="_blank"></a></span></h2>
 	<div class="row">
 		<div class="span4">
-			<textarea id="hc_input" class="pull-left input" name="hc_input"><html><head><title></title></head><body></body></html></textarea>
+			<textarea id="hc_input" class="pull-left input" name="hc_input">
+				<html>
+				<head>
+					<title></title>
+				</head>
+				<body>
+				</body>
+				</html>
+			</textarea>
 		</div>
 		<div class="span4">
 			<textarea id="hc_output" class="pull-left output" name="hc_output"></textarea>
--- compressorbot/templates/content-js-compressor.php
+++ compressorbot/templates/content-js-compressor.php
@@ -2,7 +2,11 @@
 	<h2>Compress Your JavaScript <span class="muted">powered by <a href="" target="_blank"></a></span></h2>
 	<div class="row">
 		<div class="span4">
-			<textarea id="jc_input" class="pull-left input" name="jc_input">function test(){console.log("something");}</textarea>
+			<textarea id="jc_input" class="pull-left input" name="jc_input">
+				function test(){
+					console.log("something");
+				}
+			</textarea>
 		</div>
 		<div class="span4">
 			<textarea id="jc_output" class="pull-left output" name="jc_output"></textarea>
