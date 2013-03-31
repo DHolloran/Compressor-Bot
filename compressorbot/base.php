fbd5dffd1fbdccaf1a2840a391f379246f19d365
Dan Holloran <dtholloran@gmail.com>
2013-03-31 09:46:02 -0500

    Cleaned up navbar section

--- compressorbot/base.php
+++ compressorbot/base.php
@@ -5,14 +5,8 @@
 
   <?php
     do_action('get_header');
-    // Use Bootstrap's navbar if enabled in config.php
-    if (current_theme_supports('bootstrap-top-navbar')) {
-      get_template_part('templates/header-top-navbar');
-    } else {
-      get_template_part('templates/header');
-    }
+    get_template_part('templates/header-top-navbar');
   ?>
-
   <div class="wrap container" role="document">
     <div class="content row">
       <div class="main <?php echo roots_main_class(); ?>" role="main">
