061f1739ffa6c02d18187a750123a8939d1e3ac0
Dan Holloran <dtholloran@gmail.com>
2013-03-31 09:49:09 -0500

    Set options to span3 with an offset and lowered htag sizes

--- compressorbot/templates/content-beautifier-options.php
+++ compressorbot/templates/content-beautifier-options.php
@@ -1,8 +1,8 @@
-<div class="span4 options">
+<div class="span3 offset1 options">
 	<!-- Language Options -->
 	<div class="row">
 		<div id="lb_options" class="span3 options-section">
-			<h3>Choose Your Language</h3>
+			<h4>Choose Your Language</h4>
 			<p>
 				<label><input checked type="radio" name="lb_language" class="lb-language" id="lb_cb_language" value="cb-options-section"> CSS</label>
 				<label><input type="radio" name="lb_language" class="lb-language" id="lb_jb_language" value="jb-options-section"> JS</label>
@@ -15,26 +15,26 @@
 	<!-- CSS Options -->
 	<div class="row">
 		<div class="span3">
-			<h3>Select Your Options</h3>
+			<h4>Select Your Options</h4>
 		</div>
 		<div id="cb_options" class="span3 cb-options-section options-section">
 			<p>
-				<h4>Indent</h4>
+				<h5>Indent</h5>
 				<label><input checked type="radio" name="cb_indent" class="cb-spacing" id="cb_tab" value="tab"> tab</label>
 				<label><input type="radio" name="cb_indent" class="cb-spacing" id="cb_fourspaces" value="fourspaces"> 4 spaces</label>
 				<label><input type="radio" name="cb_indent" class="cb-spacing" id="cb_twospaces" value="twospaces"> 2 spaces</label>
 			</p>
 			<p>
-				<h4>Braces</h4>
+				<h5>Braces</h5>
 				<label><input checked type="radio" name="cb_openbrace" class="cb-braces" id="cb_openbrace_separate_line" value="separate-line"> separate line</label>
 				<label><input type="radio" name="cb_openbrace" class="cb-braces" id="cb_end_of_line" value="end-of-line"> end of line</label>
 			</p>
 			<p>
-				<h4>Semicolon</h4>
+				<h5>Semicolon</h5>
 				<label><input checked type="checkbox" name="cb_autosemicolon" class="cb-auto-semicolon" id="cb_cb_autosemicolon" value="on"> Automatic semicolon</label>
 			</p>
 			<p>
-				<h4>CSS Linting</h4>
+				<h5>CSS Linting</h5>
 				<label><input checked type="checkbox" name="cb_csslint" class="cb-css-lint" id="cb_csslint" value="on"> CSS Lint</label>
 			</p>
 		</div>
@@ -45,17 +45,22 @@
 	<div class="row">
 		<div id="jb_options" class="span3 jb-options-section options-section hide">
 			<p>
-				<h4>Indent</h4>
+				<h5>Indent</h5>
 				<label><input checked type="radio" name="jb_indent_char" class="jb-indent-char" id="jb_indent_char_tab" value="tab"> tab</label>
 				<label><input type="radio" name="jb_indent_char" class="jb-indent-char" id="jb_indent_char_twospaces" value="twospaces"> two spaces</label>
 				<label><input type="radio" name="jb_indent_char" class="jb-indent-char" id="jb_indent_char_fourspaces" value="fourspaces"> four spaces</label>
 			</p>
 			<p>
-				<h4>Braces</h4>
+				<h5>Braces</h5>
 				<label><input checked type="radio" name="jb_braces" class="jb-braces" id="jb_collapse_brace" value="collapse"> collapsed</label>
 				<label><input type="radio" name="jb_braces" class="jb-braces" id="jb_expand" value="expand"> expanded</label>
 				<label><input type="radio" name="jb_braces" class="jb-braces" id="jb_end_expanded" value="end-expand"> end expanded</label>
 				<label><input type="radio" name="jb_braces" class="jb-braces" id="jb_expand_strict" value="expand-strict"> expand strict</label>
+			</p>
+			<p>
+				<h5>JS Linting</h5>
+				<!-- <label><input checked type="radio" name="jb_linting" class="jb-linting" id="jb_jslint" value="jslint"> JSLint</label> -->
+				<label><input checked type="checkbox" name="jb_linting" class="jb-linting" id="jb_jshint" value="jshint"> JSHint</label>
 			</p>
 			<p>
 				<a href="#jb_options_advanced" id="jb_options_advanced_link" class="options-advanced-link">Show Advanced Options</a>
@@ -86,11 +91,6 @@
 					<label>wrap line length<input type="number" name="jb_wrap_line_length" class="jb-wrap-line-length" id="jb_wrap_line_length" min="0" value="0"></label>
 				</p>
 			</div>
-			<p>
-				<h4>JS Linting</h4>
-				<!-- <label><input checked type="radio" name="jb_linting" class="jb-linting" id="jb_jslint" value="jslint"> JSLint</label> -->
-				<label><input checked type="checkbox" name="jb_linting" class="jb-linting" id="jb_jshint" value="jshint"> JSHint</label>
-			</p>
 		</div> <!-- /#jb_options -->
 	</div> <!-- /.row -->
 	<!-- END JS Options -->
@@ -99,19 +99,19 @@
 	<div class="row">
 		<div id="hb_options" class="span3 hb-options-section options-section hide">
 			<p>
-				<h4>Indent</h4>
+				<h5>Indent</h5>
 				<label><input checked type="radio" name="hb_indent_char" class="hb-indent-char" id="hb_indent_char_tab" value="tab"> tab</label>
 				<label><input type="radio" name="hb_indent_char" class="hb-indent-char" id="hb_indent_char_twospaces" value="twospaces"> two spaces</label>
 				<label><input type="radio" name="hb_indent_char" class="hb-indent-char" id="hb_indent_char_fourspaces" value="fourspaces"> four spaces</label>
 			</p>
 			<p>
-				<h4>Braces</h4>
+				<h5>Braces</h5>
 				<label><input checked type="radio" name="hb_braces" class="hb-braces" id="hb_collapse_brace" value="collapse"> collapsed</label>
 				<label><input type="radio" name="hb_braces" class="hb-braces" id="hb_expand" value="expand"> expanded</label>
 				<label><input type="radio" name="hb_braces" class="hb-braces" id="hb_end_expanded" value="end-expand"> end expanded</label>
 			</p>
 			<p>
-				<h4>Indent Scripts</h4>
+				<h5>Indent Scripts</h5>
 				<label><input checked type="radio" name="hb_indent_scripts" class="hb-indent-scripts" id="hb_normal" value="normal"> normal</label>
 				<label><input type="radio" name="hb_indent_scripts" class="hb-indent-scripts" id="hb_keep" value="keep"> keep</label>
 				<label><input type="radio" name="hb_indent_scripts" class="hb-indent-scripts" id="hb_seperate" value="seperate"> seperate</label>
--- compressorbot/templates/content-minifier-options.php
+++ compressorbot/templates/content-minifier-options.php
@@ -1,8 +1,8 @@
-<div class="span4 options">
+<div class="span3 offset1 options">
 	<!-- Language Options -->
 	<div class="row">
 		<div id="lm_options" class="span3 options-section">
-			<h3>Choose Your Language</h3>
+			<h4>Choose Your Language</h4>
 			<p>
 				<label><input checked type="radio" name="lm_language" class="lm-language" id="lm_cm_language" value="cm-options-section"> CSS</label>
 				<!-- <label><input type="radio" name="lm_language" class="lm-language" id="lm_jm_language" value="jm-options-section"> JS</label> -->
@@ -15,11 +15,11 @@
 	<!-- CSS Options -->
 	<div class="row">
 		<div class="span3">
-			<h3>Select Your Options</h3>
+			<h4>Select Your Options</h4>
 		</div>
 		<div id="cm_options" class="span3 cm-options-section options-section">
 			<p>
-				<h4>CSS Linting</h4>
+				<h5>CSS Linting</h5>
 				<label><input checked type="checkbox" name="cm_csslint" class="cm-css-lint" id="cm_csslint" value="on"> CSS Lint</label>
 			</p>
 		</div>
@@ -36,7 +36,7 @@
 	<div class="row">
 		<div id="hm_options" class="span3 hm-options-section options-section hide">
 			<p>
-				<h4>Comments</h4>
+				<h5>Comments</h5>
 				<label><input checked type="checkbox" name="hb_strip_comments" class="hb-strip-comments" id="hb_strip_comments" value="on"> Strip comments</label>
 			</p>
 		</div> <!-- /#hm_options -->
