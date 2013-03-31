cfbf7fa2c4d516cf8d289d67e6072031fe890a39
Dan Holloran <dtholloran@gmail.com>
2013-03-30 19:58:25 -0500

    Removed all legacy code

--- _assets/includes/helpers/sandbox.php
+++
@@ -1,7 +0,0 @@
-<?php
-	require_once "functions.php";
-
-	$validate = validateFile('http://compressorbot.com/tests/test.html');
-	echo "/*"."\r\n" . $validate . "*/" . "\r\n" . "\r\n";
-	echo "/*\r\n" . $validate . "*/" . "\r\n" . "\r\n";
-	echo "<!--\r\n" . $validate . "-->" . "\r\n" . "\r\n";
--- index.php
+++ index.php
@@ -1,21 +1,2 @@
 <?php
-	require_once("_assets/includes/controller/PageView.php");
-	// ========== Home Page =============
-	$view = new PageView();
-	// Require Header
-	echo '<div id="wrap">';
-		$view->show('header','partials','Home');
-
-		// Require Modals
-		$view->show('contact_modal','modals'); // Contact Us Modal
-		$view->show('edit_modal','modals'); // Edit Profile Modal
-		$view->show('register_modal','modals'); // Register Modal
-
-		// Require Body
-		echo '<div id="main">';
-			$view->show('home_page','body');
-		echo '</div><!-- /#main -->';
-	echo '</div> <!-- End Sticky Footer #wrap -->';
-	// Require Footer
-	$view->show('footer','partials');
-
+// Silence is golden.
